98b2340f725ce4cbab2e07525086dc8899da5304
Sack <rauldaxo@gmail.com>
2024-06-11 20:43:16 -0600

    Fixed double jumps problem
    Added a isOnGround variable to act with a new method called OnCollisionEnter that changes when the player is touching the ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce = 10;
+    public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter()
+    {
+        isOnGround = true;
     }
 }
