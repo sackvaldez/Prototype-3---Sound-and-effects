de9eb9c247ca6318419db862b47e7c50b6327211
Sack <rauldaxo@gmail.com>
2024-06-13 13:17:28 -0600

    Optimized Spawn Script
    Now the Instantiation goes by a new method that is called in the Start function with InvokeRepeating
    
    Also, in RigidBody constraints, we freeze all X and Z axes to avoid player to turn or move in an unwanted direction

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,11 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     // Start is called before the first frame update
     void Start()
     {
-
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("spawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
@@ -18,4 +19,10 @@ public class SpawnManager : MonoBehaviour
     {
 
     }
+    void spawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+
+
 }
