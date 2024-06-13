ac2ab1732188ac1dbc310eafe543a8c4371f3a10
Sack <rauldaxo@gmail.com>
2024-06-13 15:37:52 -0600

    Stop MoveLeft script when GameOver
    We connected scripts with variable using GameObject.Find and GetComponent to use the PlayerController script

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 20;
+    // Variable to connect with another script
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-
+        // Find the "Player" gameObject to get its component, in this case, the "PlayerController" script
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // X axis movement using time and speed variable
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        // While gameOver is not true, keep moving left
+        if (playerControllerScript.gameOver == false)
+        {
+            // X axis movement using time and speed variable
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
     }
 }
